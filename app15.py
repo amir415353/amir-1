5b12e2ee443aaa0122e1040b0b4b0f96c3c46f78
amir20172018 <59166934+amir20172018@users.noreply.github.com>
2020-10-03 00:31:17 +0330

    Add files via upload

---
+++ app15.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+#time to refresh page (seconds)
+Timer = 20
+#youtube link
+link = input('please enter your link: ')
+driver = webdriver.Firefox()
+driver.get(link)
+
+while True:
+    time.sleep(Timer)
+    driver.quit()
+    print('done')
